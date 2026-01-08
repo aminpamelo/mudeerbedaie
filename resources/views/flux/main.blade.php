a4cda7de47846d198dee8ad785f025f6af32a4c9
aminpamelo <aminsuhardi96@gmail.com>
2026-01-08 11:26:02 +0800

    fix the padding

--- resources/views/flux/main.blade.php
+++ resources/views/flux/main.blade.php
@@ -4,7 +4,7 @@
 
 @php
 $classes = Flux::classes('[grid-area:main]')
-    ->add('p-6 lg:p-8')
+    ->add('p-3 sm:p-4 lg:p-8')
     ->add('w-full min-w-0')
     ->add('[[data-flux-container]_&]:px-0') // If there is a wrapping container, let IT handle the x padding...
     ->add($container ? 'mx-auto [:where(&)]:max-w-7xl' : '')
